031371c614bff4278d93c55446d8cc369a1f26df
Arpit Agarwal <arpitmanas28@gmail.com>
2024-09-15 22:57:37 +0530

    Added the feature text area

--- frontend/src/screens/Home.screen.tsx
+++ frontend/src/screens/Home.screen.tsx
@@ -1,11 +1,13 @@
 import Hero from '../container/Hero/Hero.container';
 import Header from '../container/Header/Header.container';
+import Features from '../container/Features/Features.container';
 
 const Home = () => {
   return (
     <>
       <Header />
       <Hero />
+      <Features />
     </>
   );
 };
