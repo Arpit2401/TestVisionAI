fa7885f01d8cd49c9404754cc816e4b903263983
Arpit Agarwal <arpitmanas28@gmail.com>
2024-09-15 16:12:31 +0530

    Added feature cards and it's layout

---
+++ frontend/src/components/FeatureCard/FeatureCard.component.tsx
@@ -0,0 +1,31 @@
+import { Box, Typography } from '@mui/material';
+
+import { FeatureCardProps } from './FeatureCard.types';
+
+const FeatureCard: React.FC<FeatureCardProps> = ({
+  featureText,
+  Icon,
+  backgroundColor,
+}) => {
+  return (
+    <Box
+      padding="40px"
+      display="flex"
+      alignItems="center"
+      flexDirection="column"
+      textAlign="center"
+      gap="20px"
+      color="white"
+      fontWeight="bold"
+      borderRadius="24px"
+      sx={{
+        background: backgroundColor,
+      }}
+    >
+      <Icon fontSize="large" />
+      <Typography fontWeight="bold">{featureText}</Typography>
+    </Box>
+  );
+};
+
+export default FeatureCard;
---
+++ frontend/src/components/FeatureCard/FeatureCard.types.ts
@@ -0,0 +1,7 @@
+import { SvgIconComponent } from '@mui/icons-material';
+
+export type FeatureCardProps = {
+  featureText: string;
+  Icon: SvgIconComponent;
+  backgroundColor: string;
+};
